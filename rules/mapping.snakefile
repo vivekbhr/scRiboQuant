6bea89d9d821b65091daeb83ec2d6a155e1566c8
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2020-03-18 16:04:26 +0100

    tools added

--- rules/mapping.snakefile
+++ rules/mapping.snakefile
@@ -1,37 +1,61 @@
-prep_annotation:
+gtf=/hpc/hub_oudenaarden/vbhardwaj/annotations/hg38_gencode31_extra/annotation/genes.gtf
+idx=../annotation/STARindex/
+barcodes=../whitelist.txt
+r1=../fastq_reformatted/MAV-RPFv4-HEK293T_R1.fastq.gz
+r2=../fastq_reformatted/MAV-RPFv4-HEK293T_R2.fastq.gz
+prefix=RPFv4_
+fa=
+
+## generate genome
+STAR --runThreadN 10 \
+--runMode genomeGenerate \
+--genomeDir STARindex \
+--genomeFastaFiles $fa \
+--sjdbGTFfile $gtf \
+--sjdbOverhang 50
+
+## preprocess fastq
+
+~/programs/icetea/trimFastq test_R1.fastq test_R2.fastq out_R1.fastq.gz out_R2.fastq.gz
+
+## map
+STAR --runThreadN 20 \
+  --sjdbOverhang 50 \
+  --outSAMtype BAM SortedByCoordinate \
+  --outSAMattributes NH HI AS nM CB UB \
+  --sjdbGTFfile ${gtf} \
+  --genomeDir ${idx} \
+  --readFilesIn  ${r1} ${r2} \
+  --readFilesCommand gunzip -c \
+  --outFileNamePrefix ${prefix} \
+  --soloType CB_UMI_Simple \
+  --soloFeatures Gene \
+  --soloUMIstart 11 \
+  --soloUMIlen 10 \
+  --soloCBstart 1 \
+  --soloCBlen 10 \
+  --soloCBwhitelist ${barcodes} \
+  --soloBarcodeReadLength 0 \
+  --soloCBmatchWLtype 1MM \
+  --soloStrand Forward \
+  --soloUMIdedup Exact \
+  --soloUMIfiltering MultiGeneUMI \
+  --quantMode TranscriptomeSAM \
+  --quanTranscriptomeBam Singleend \# breaks RSEM compatibility, but gives more reads
+  --clip5pNbases 10 0 # only for current reformatted fq
 
 
-rule STAR:
-    input:
-    fastq_dir+"/{sample}"+reads[0]+".fastq.gz"
-    output:
-    bam = temp(aligner+"/{sample}.sorted.bam")
-    params:
-    alignerOptions = str(alignerOptions or ''),
-    gtf = genes_gtf,
-    index = star_index,
-    prefix = aligner+"/{sample}/{sample}.",
-    samsort_memory = '2G',
-    sample_dir = aligner+"/{sample}",
-    samtools_threads = 5
-    benchmark:
-    aligner+"/.benchmark/STAR.{sample}.benchmark"
-    threads: 20  # 3.2G per core
-    conda: CONDA_RNASEQ_ENV
-    shell:
-        """
-        MYTEMP=$(mktemp -d ${{TMPDIR:-/tmp}}/snakepipes.XXXXXXXXXX)
-        ( [ -d {params.sample_dir} ] || mkdir -p {params.sample_dir} )
-        STAR --runThreadN {threads} \
-            {params.alignerOptions} \
-            --sjdbOverhang 100 \
-            --outSAMunmapped Within \
-            --outSAMtype BAM Unsorted \
-            --outStd BAM_Unsorted \
-            --sjdbGTFfile {params.gtf} \
-            --genomeDir {params.index} \
-            --readFilesIn <(gunzip -c {input}) \
-            --outFileNamePrefix {params.prefix} \
-            | samtools sort -m {params.samsort_memory} -T $MYTEMP/{wildcards.sample} -@ {params.samtools_threads} -O bam -o {output.bam} -
-            rm -rf $MYTEMP
-        """
+## umi dedup of the mapped file
+bam=../STARout/Starv1-Leu6h_Aligned.toTranscriptome.sorted.bam
+Aligned.sortedByCoord.out.bam
+prefix=Starv1-Leu6h
+
+# index
+samtools index
+# dedup
+
+umi_tools dedup --mapping-quality 20 \
+--per-cell --umi-tag=UB --cell-tag=CB --extract-umi-method=tag \
+--method unique --spliced-is-unique \
+--output-stats=${prefix}.stats \
+-I ${bam} -L ${prefix}.log > ${prefix}.bam
---
+++ tools/trimFastq/trimFastq.c
@@ -0,0 +1,139 @@
+#include <zlib.h>
+#include <stdio.h>
+#include "kseq.h"
+#include <string.h>
+//
+#include "trimFastq.h"
+
+// THIS PROGRAM WILL TAKE TWO FASTA FILES (R1 & R2) AND TRIM THE UMI sequence OFF R1, adding it in R2
+
+//copy the barcode/UMI sequence (position 0 to 9)
+void copy_seq(char d[], char s[]) {
+   int bcstrt = 0;
+   int bcend = 10;
+   //copy first 10 bases
+   while (bcstrt < bcend) {
+      d[bcstrt] = s[bcstrt];
+      bcstrt++;
+   }
+   d[bcstrt] = '\0';
+}
+
+//trim the seq off the barcode and index (10 bases removed)
+void trim_seq(char d[], char s[]) {
+   int idx = 10;
+   int end = strlen(s);
+   int i = 0;
+   //copy first five bases
+   while (idx < end) {
+      d[i] = s[idx];
+      i++;
+      idx++;
+   }
+   d[i] = '\0';
+}
+
+// make header in UMItools format
+//void make_header(char * output, char seq[], char tseq[]) {
+//  char um[10];
+//  char bc[10];
+//  output[0] = '\0';
+//  copy_seq(um,seq);
+//  copy_seq(bc,tseq);
+//  strcat(output,bc);
+//  strcat(output,"_");
+//  strcat(output,um);
+//}
+
+// put them together
+KSEQ_INIT(gzFile, gzread);
+
+int main(int argc, char *argv[]) //main
+{
+	gzFile fp;
+  gzFile fpt;
+	gzFile fpout;
+  gzFile fptout;
+
+	kseq_t *seq;
+  kseq_t *seqt;
+
+	int l;
+	if (argc == 1) {
+		fprintf(stderr, "Usage: %s <in_R1.fastq.gz> <in_R2.fastq.gz> <out_R1.fastq> <out_R2.fastq>\n", argv[0]);
+		return 1;
+	}
+
+	fp = gzopen(argv[1], "r"); // file pointer for R1
+  fpt = gzopen(argv[2], "r"); // file pointer for R2
+
+	fpout = gzopen(argv[3], "wb"); // output file pointer for R1
+  fptout = gzopen(argv[4], "wb"); // output file pointer for R2
+
+	seq = kseq_init(fp);
+  seqt = kseq_init(fpt);
+// save data for R1
+	char * sequence;
+  char * name;
+  char * qual;
+  char * comment;
+// for R2
+  char * tsequence;
+  char * tname;
+  char * tqual;
+  char * tcomment;
+
+
+  char umiseq[10];
+  char umiqual[10];
+  char barcode[20];
+  char trimseq[100];
+  char trimqual[100];
+
+	while ((l = kseq_read(seq)) >= 0) {
+    kseq_read(seqt);
+		sequence = seq->seq.s;
+    // other components
+    name = seq->name.s;
+    if (seq->qual.l) qual = seq->qual.s;//quality
+    if (seq->comment.l) comment = seq->comment.s;//comment
+    // for file R2
+    tsequence = seqt->seq.s;
+    tname = seqt->name.s;
+    if (seqt->qual.l) tqual = seqt->qual.s;//quality
+    if (seqt->comment.l) tcomment = seqt->comment.s;//comment
+
+    //now print data for both R1 and R2
+
+    copy_seq(umiseq,sequence); // copy UMI from R1
+    copy_seq(barcode,tsequence); // copy BC from R2
+    trim_seq(trimseq,sequence);
+
+    // header = read name + _BC_UMI + comment
+    gzprintf(fpout,"@%s_%s_%s %s\n", name, barcode, umiseq, comment);
+    gzprintf(fptout,"@%s_%s_%s %s\n", tname, barcode, umiseq, tcomment);
+
+    gzprintf(fpout,"%s\n", trimseq);//trimmed seq for R1
+    gzprintf(fptout,"%s%s\n", tsequence, umiseq);//normal seq for R2 + UMI from R1
+
+    gzprintf(fpout,"%s\n","+" );
+    gzprintf(fptout,"%s\n","+" );
+
+    copy_seq(umiqual,qual); // copy UMI quality from R1
+    trim_seq(trimqual,qual);
+    gzprintf(fpout,"%s\n", trimqual);//trimmed qual for R1
+    gzprintf(fptout,"%s%s\n", tqual, umiqual);//normal qual for R2 + UMI qual:
+	}
+
+	kseq_destroy(seq);
+  kseq_destroy(seqt);
+
+	gzclose(fp);
+  gzclose(fpt);
+
+  gzclose(fpout);
+  gzclose(fptout);
+
+  return 0;
+
+}
